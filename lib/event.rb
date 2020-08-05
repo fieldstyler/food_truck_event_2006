9b92e6ac1813e10b02da0c6635db8b57a3ae9371
fieldstyler <tdfields93@gmail.com>
2020-08-05 10:35:14 -0600

    Add create_item_hash helper method and total_inventory method

--- lib/event.rb
+++ lib/event.rb
@@ -22,4 +22,25 @@ class Event
       truck.inventory.include?(item)
     end
   end
+
+  def total_inventory
+    total_inventory_hash = Hash.new
+    item_hash = Hash.new
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item|
+        total_inventory_hash[item[0]] = create_item_hash(item[0])
+      end
+    end
+    total_inventory_hash
+  end
+
+  def create_item_hash(item)
+    {
+      :quantity => @food_trucks.sum do |truck|
+                    truck.check_stock(item)
+                  end,
+      :food_trucks => food_trucks_that_sell(item)
+    }
+  end
+
 end
