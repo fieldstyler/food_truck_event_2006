a1d2fd9527b963851bc11958daefb12ca4d00fce
fieldstyler <tdfields93@gmail.com>
2020-08-05 09:08:54 -0600

    Add test it has attributes test and passes

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -15,7 +15,8 @@ class FoodTruckTest < Minitest::Test
     assert_instance_of FoodTruck, @food_truck
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+  end
 
 end
