f3cde80ba483b77beef3aaf32894e89e34bab62a
fieldstyler <tdfields93@gmail.com>
2020-08-05 09:13:38 -0600

    Add stock and check stock methods and set inventory has to default value of 0

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,7 +4,19 @@ class FoodTruck
               :inventory
   def initialize(name)
     @name = name
-    @inventory = Hash.new
+    @inventory = Hash.new{0}
   end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+  end
+
+
+
+
 
 end
